9ea2a4722b5684de23d635bab55e29dc10fec4ee
Marse9713 <raffaele.marseu@outlook.it>
2022-12-08 10:32:03 +0100

    inserimento della pagina iniziale successiva al login

--- webapp/login/index.js
+++ webapp/login/index.js
@@ -3,6 +3,6 @@ let btn = document.getElementById("submit");
 btn.addEventListener("click", (e) => {
 
     window.alert("Login effettuato");
-    window.location = "paginadiprova.html";
+    window.location = "webapp/pagina_iniziale.htmle";
 
 });
